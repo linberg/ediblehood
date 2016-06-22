7fea0669fc60eafa869aba122a59fcd41dca6a41
kaja <kaja@Kajas-MacBook.local>
2016-06-22 10:08:09 +0200

    regular expression for email validation and title uniqueness

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 class Location < ActiveRecord::Base
   validates_presence_of :email, :title, :latitude, :longitude
   validates :email, format: /\A\w+\S+[@]\w+\S+[.]\w+\S+\z/
-  		
+  validates :title, uniqueness: true
+  validates :latitude, :longitude, uniqueness: true
+	
 end
 
