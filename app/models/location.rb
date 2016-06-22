cf0c75035c174cf91f77235ed397ab2ca31e8ba6
kaja <kaja@Kajas-MacBook.local>
2016-06-22 13:56:24 +0200

    update validation criteria for db input
    title doesn’t need to be unique. location will be unique anyway, if the
    marker is not exactly right on the same spot. title should be longer
    than just one character

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,7 @@
 class Location < ActiveRecord::Base
   validates_presence_of :email, :title, :latitude, :longitude
-  validates :email, format: /\A\w+\S+[@]\w+\S+[.]\w+\S+\z/
-  validates :title, uniqueness: true
-  validates :latitude, :longitude, uniqueness: true
-	
+  validates :email, format: /\A\w+\S+[@]\w+\S+[.]\w+\S+\z/ 
+  validates :title, format: string.lenght: >1
 end
 
+# /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ could be another regular expression for the email-adress, but not neccesary
