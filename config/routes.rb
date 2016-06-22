92eb58b9c996864595571a422b790c66abefeafd
kaja <kaja@Kajas-MacBook.local>
2016-06-22 17:57:58 +0200

    set first about as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'first/about'
+  get 'locations/new'
+
   resources :locations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'first#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
