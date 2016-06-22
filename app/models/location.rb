7d4ecb8449b2ffd0ab48d0bfa00338f099dd68ed
kaja <kaja@Kajas-MacBook.local>
2016-06-22 15:14:47 +0200

    taking out the string length cause it doesn't work

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ActiveRecord::Base
   validates_presence_of :email, :title, :latitude, :longitude
   validates :email, format: /\A\w+\S+[@]\w+\S+[.]\w+\S+\z/ 
-  validates :title, format: string.lenght: >1
 end
 
 # /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ could be another regular expression for the email-adress, but not neccesary
