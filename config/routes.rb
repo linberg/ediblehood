036b1727401d486ee1e3a5efda1bac3ccbf46da5
kaja <kaja@Kajas-MacBook.local>
2016-06-23 13:56:16 +0200

    tried to put routes to the home location list page, but it doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'first/about'
   get 'locations/new'
+  get 'home/location_list'
 
-  resources :locations
+  resources :locations, :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'location_list/:id' => 'home#location_list'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
